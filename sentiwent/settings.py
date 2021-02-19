42863717294e1086c48beac5ff6814ea30148332
Kahraman Bayraktar <adm-kbayraktar@coresvc.net>
2021-02-20 00:52:13 +0300

    Updated settings.py

--- sentiwent/settings.py
+++ sentiwent/settings.py
@@ -131,8 +131,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
