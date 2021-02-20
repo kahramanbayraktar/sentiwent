ebf9c4d54fbe4f7e4c98e19e896c921f0f23af98
Kahraman Bayraktar <adm-kbayraktar@coresvc.net>
2021-02-20 20:09:34 +0300

    Set static url to an Azure blob container.

--- sentiwent/settings.py
+++ sentiwent/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'https://swe573storage.blob.core.windows.net/swe573container/'
 if DEBUG:
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static')
